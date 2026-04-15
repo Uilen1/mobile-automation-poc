1b2c245b2a66bd54fa981977af3bfee7da79a8b5
Uilen1 <uilenlelles@hotmail.com>
2026-04-15 18:27:35 -0300

    refactor: ajustando as configurações para nao ter screenshot automatico

--- wdio.mobile.matterport.conf.ts
+++ wdio.mobile.matterport.conf.ts
@@ -30,7 +30,8 @@ export const config = {
     framework: 'mocha',
     reporters: ['spec', ['allure', {
         outputDir: 'allure-results',
-        disableWebdriverStepsReporting: true
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: true
     }]],
     mochaOpts: {
         ui: 'bdd',
@@ -47,11 +48,13 @@ export const config = {
     },
 
     capabilities: [{
-        platformName: 'Android',
-        'appium:deviceName': 'RQ8R70G7AJD',
-        'appium:platformVersion': '13',
-        'appium:automationName': 'UiAutomator2',
-        'appium:autoGrantPermissions': true,
-        'appium:appPackage': 'com.matterport.android.capture'
+        platformName: 'Android', // Platform name for Appium
+        'appium:deviceName': 'RQ8R70G7AJD', // Update to match your emulator/device name (use `adb devices` to find it)
+        'appium:platformVersion': '13', //  Update to match your emulator/device version
+        'appium:automationName': 'UiAutomator2', // Preferred automation engine for Android
+        'appium:autoGrantPermissions': true, // Automatically grants all permissions on app install
+        'appium:noReset': false, // Ensures app data is cleared for a fresh start
+        'appium:fullReset': false, // Avoids uninstalling the app after tests
+        'appium:appPackage': 'com.matterport.android.capture' // Update to your app's package name
     }]
 };
